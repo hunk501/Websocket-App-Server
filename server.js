9aec2a017f891a337f0179169048287b51562c3e
Dennis Lira <dlira@stratpoint.com>
2022-10-12 23:35:32 +0800

    Add Base64 module to encode and decode base64 string

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const WebSocketServer = require('ws').Server;
 const WebsocketStream = require('websocket-stream');
+const { Readable } = require('stream');
+const { Base64Encode, Base64Decode } = require('base64-stream');
 
 const wss = new WebSocketServer({ port: 8989 });
 //wss.binaryType = 'blob';
@@ -9,16 +11,24 @@ wss.on('connection', (socket) => {
   //socket.send("Hello from server");
 
   socket.on('message', (msg) => {
-    console.log('Msg: ', msg.toString());
+    console.log('Msg: Incoming');
     // send reply
-    setTimeout(() => {
-      //socket.send(`Server ${new Date().toISOString()}`);
+    // setTimeout(() => {
+    //   //socket.send(`Server ${new Date().toISOString()}`);
+    //   const streams = WebsocketStream(socket);
+    //   const rs = fs.createReadStream("./files/image2.png");
+    //   rs.on('data', () => console.log('Reading file'));
+    //   //rs.setEncoding('base64');
+    //   rs.pipe(streams);
+    // }, 3000);
+
+    //socket.send(msg);
+    if(msg.toString().length > 0) {
       const streams = WebsocketStream(socket);
-      const rs = fs.createReadStream("../files/image1.png");
-      rs.on('data', () => console.log('Reading file'));
-      //rs.setEncoding('base64');
-      rs.pipe(streams);
-    }, 3000);
+      const r = Readable.from(msg);
+      r.pipe(new Base64Decode() ).pipe(streams);
+    }
+
   });
   socket.on('close', () => console.log('Client disconnected'));
 });
